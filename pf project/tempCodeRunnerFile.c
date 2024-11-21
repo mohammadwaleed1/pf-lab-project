fb43375db9d7242acc2f60114fa8f21b03c90dce
mohammadwaleed1 <waleedmohammad484@gmail.com>
2024-11-21 18:28:18 +0500

    Delete pf project/tempCodeRunnerFile.c

--- pf project/tempCodeRunnerFile.c
+++
@@ -1,7 +0,0 @@
-
-        // Move to the next word in the category
-        wordIndex = wordIndex + 1;  // Wrap around if reaching the end
-    }
-    printf("\nTime's up! You guessed %d words correctly & %d incorrectly.\n", correctWordCount,incorrectwordcount);
-    return 0;
-}
